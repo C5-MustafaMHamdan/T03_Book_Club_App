def4ca49b68d8d7d027e711d5e3e944617b9842a
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-07-24 18:53:50 +0300

    build data base + env file

--- models/schema.sql
+++ models/schema.sql
@@ -1,2 +1,46 @@
 -- Build Your Tables Here --
 
+DROP DATABASE T03_Book_Club_App;
+
+CREATE DATABASE T03_Book_Club_App;
+
+USE T03_Book_Club_App;
+
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    role VARCHAR(255) NOT NULL UNIQUE,
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE permissions (
+    id INT AUTO_INCREMENT NOT NULL,
+    permission VARCHAR(255) NOT NULL,
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles_permissions (
+    id INT NOT NULL AUTO_INCREMENT NOT NULL,
+    role_id INT,
+    permission_id INT,
+    is_deleted TINYINT DEFAULT 0,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (permission_id) REFERENCES permissions (id),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
